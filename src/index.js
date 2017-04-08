9ac8af4a83f6cf197f80ab27394a72ee7df01c3d
Yuri Kilian <kilian.inf@gmail.com>
2017-04-08 02:25:05 -0300

    Scss adjusts

--- src/index.js
+++ src/index.js
@@ -4,11 +4,9 @@ import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Website from './website/Website';
 
-import './common/css/reset.css';
-import './common/css/bootstrap.css';
-import './common/css/index.css';
+import './common/css/common.css';
+import Website from './website/Website';
 
 const Admin = () => <h1>Admin</h1>;
 
