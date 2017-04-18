2f4031fede1438b75940d6fc40208f8264fc5dcd
Yuri Kilian <kilian.inf@gmail.com>
2017-04-17 22:44:42 -0300

    Added form components

--- src/index.js
+++ src/index.js
@@ -2,23 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
-
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import { Provider } from 'react-redux';
+import store from './store';
 
 import './common/css/common.css';
 import Website from './website/Website';
 
 const Admin = () => <h1>Admin</h1>;
-
 const App = () => (
-    <Router>
-        <section>
-            <Switch>
-                <Route exact path="/admin" component={Admin}/>
-                <Route path="/" component={Website}/>
-            </Switch>
-        </section>
-    </Router>
+    <Provider store={store}>
+        <Router>
+            <section>
+                <Switch>
+                    <Route exact path="/admin" component={Admin}/>
+                    <Route path="/" component={Website}/>
+                </Switch>
+            </section>
+        </Router>
+    </Provider>
 );
 
 ReactDOM.render(
